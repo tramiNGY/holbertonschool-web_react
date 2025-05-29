bb7ef3de36e50cab96b408a8b5ca6d16318b9bef
tramiNGY <ngytrami@gmail.com>
2025-05-29 20:48:23 +0200

    creating functions specific to employees

--- TypeScript/task_2/js/main.ts
+++ TypeScript/task_2/js/main.ts
@@ -45,3 +45,19 @@ interface DirectorInterface {
   console.log(createEmployee(1000).constructor.name);
   console.log(createEmployee('$500').constructor.name);
   
+
+  function isDirector(employee: Director | Teacher): employee is Director {
+    return (employee as Director).workDirectorTasks !== undefined;
+  }
+  
+  function executeWork(employee: Director | Teacher): string {
+    if (isDirector(employee)) {
+      return employee.workDirectorTasks();
+    }
+    return employee.workTeacherTasks();
+  }
+  
+
+  console.log(executeWork(createEmployee(200)));
+  console.log(executeWork(createEmployee(1000)));
+  
