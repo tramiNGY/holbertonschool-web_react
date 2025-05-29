324f6f267596eac33985a5ec5f3c38c9258e5b4f
tramiNGY <ngytrami@gmail.com>
2025-05-29 20:35:14 +0200

    writing a class

--- TypeScript/task_1/js/main.ts
+++ TypeScript/task_1/js/main.ts
@@ -15,6 +15,27 @@ interface printTeacherFunction {
   (firstName: string, lastName: string): string;
 }
 
+interface StudentConstructor {
+  new (firstName: string, lastName: string): StudentClassInterface;
+}
+
+interface StudentClassInterface {
+  workOnHomework(): string;
+  displayName(): string;
+}
+
+class StudentClass implements StudentClassInterface {
+  constructor(private firstName: string, private lastName: string) {}
+
+  workOnHomework(): string {
+    return "Currently working";
+  }
+
+  displayName(): string {
+    return this.firstName;
+  }
+}
+
 const teacher3: Teacher = {
   firstName: 'John',
   lastName: 'Doe',
@@ -35,6 +56,10 @@ const printTeacher: printTeacherFunction = (firstName, lastName) => {
   return `${firstName.charAt(0)}. ${lastName}`;
 };
 
+const student = new StudentClass('Alice', 'Johnson');
+
 console.log(teacher3);
 console.log(director1);
 console.log(printTeacher("John", "Doe"));
+console.log(student.displayName());
+console.log(student.workOnHomework());
